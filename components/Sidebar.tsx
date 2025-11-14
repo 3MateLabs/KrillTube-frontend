939798739edb3ae26a5358c58432606056f38624
Cyber <shaibuafeez@gmail.com>
2025-11-14 07:15:38 +0100

    Make sidebar scrollable and update user profile image

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -27,13 +27,14 @@ export function Sidebar({ isOpen = true, onClose }: SidebarProps) {
         className={`
           fixed top-0 left-0 bottom-0 z-50 w-72 h-screen
           bg-[#0668A6] shadow-[0_4px_15px_rgba(42,42,42,0.31)] border-r-[3px] border-black backdrop-blur-[100px]
+          overflow-y-auto
           transition-all duration-300 ease-in-out
           lg:translate-x-0
           ${isOpen ? 'translate-x-0' : '-translate-x-full'}
         `}
       >
         {/* Logo */}
-        <div className="w-56 left-[29px] top-[18px] absolute inline-flex justify-start items-center gap-3">
+        <div className="w-56 mx-[29px] mt-[18px] inline-flex justify-start items-center gap-3">
           <div className="p-2 bg-black rounded-[32px] shadow-[3px_3px_0_0_black] outline outline-1 outline-offset-[-1px] outline-white inline-flex justify-center items-center">
             <div className="p-2 bg-black rounded-[32px] inline-flex justify-center items-center">
               <Image src="/logos/hamburger-menu.svg" alt="Menu" width={24} height={24} className="w-6 h-6" />
@@ -47,7 +48,7 @@ export function Sidebar({ isOpen = true, onClose }: SidebarProps) {
         </div>
 
         {/* Navigation */}
-        <div className="w-56 left-[29px] top-[111px] absolute inline-flex flex-col justify-start items-start gap-4">
+        <div className="w-56 mx-[29px] mt-[17px] mb-6 inline-flex flex-col justify-start items-start gap-4">
           {/* Main Menu */}
           <div className="self-stretch px-4 py-8 bg-[#FFEEE5] rounded-3xl outline outline-[3px] outline-offset-[-3px] outline-black backdrop-blur-[9.45px] flex flex-col justify-center items-center gap-2.5">
             <div className="self-stretch flex flex-col justify-center items-start gap-4">
@@ -181,11 +182,9 @@ export function Sidebar({ isOpen = true, onClose }: SidebarProps) {
           </div>
 
           {/* User Profile */}
-          <div className="self-stretch inline-flex justify-start items-start gap-3">
-            <div className="w-12 h-12 px-3 py-px bg-black rounded-3xl shadow-[3px_3px_0_0_black] outline outline-1 outline-offset-[-1px] outline-white flex justify-center items-center gap-2.5">
-              <div className="w-12 h-12 relative">
-                <img className="w-12 h-12 left-0 top-0 absolute rounded-full border-2 border-white" src="https://via.placeholder.com/50" alt="User" />
-              </div>
+          <div className="self-stretch inline-flex justify-start items-center gap-3">
+            <div className="w-12 h-12 bg-black rounded-full shadow-[3px_3px_0_0_black] outline outline-1 outline-offset-[-1px] outline-white flex justify-center items-center overflow-hidden">
+              <Image className="w-full h-full object-cover" src="/logos/eason.svg" alt="User" width={50} height={50} />
             </div>
             <div className="flex-1 p-2 bg-black rounded-[32px] outline outline-1 outline-offset-[-1px] outline-white inline-flex flex-col justify-start items-start gap-2.5">
               <div className="self-stretch p-2 bg-black rounded-[32px] inline-flex justify-center items-center gap-2.5">
